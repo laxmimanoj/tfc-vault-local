3d3fdcf0d9fc86d0b87e39c42bb71bceac6b7016
Manoj Poonati <manojpoonati@gmail.com>
2021-09-02 00:02:55 -0500

    loading public key from file vs inline

--- auth.tf
+++ auth.tf
@@ -9,36 +9,16 @@
 
 #configure jwt auth
 resource "vault_jwt_auth_backend" "psjwt" {
-  path        = var.root_path
-  type        = "jwt"
-  description = var.auth_backend_desccription
-  jwt_validation_pubkeys = [<<EOT
------BEGIN CERTIFICATE-----
-MIIC9DCCAdwCCQCA+pMeksBbAjANBgkqhkiG9w0BAQsFADA8MQswCQYDVQQGEwJV
-UzELMAkGA1UECAwCVFgxEDAOBgNVBAcMB0hvdXN0b24xDjAMBgNVBAMMBW1hbm9q
-MB4XDTIxMDkwMjAwMDcyMFoXDTMxMDgzMTAwMDcyMFowPDELMAkGA1UEBhMCVVMx
-CzAJBgNVBAgMAlRYMRAwDgYDVQQHDAdIb3VzdG9uMQ4wDAYDVQQDDAVtYW5vajCC
-ASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAMsn9sw3hsKwXMrrRRIJLIBn
-UZ5DKMTtO+glBhBRRIPCOirNy4zVUAXUCc/Jm5xkLYjB9RYiSzMV8/10ZKtZmhor
-U5surw1buiUv6v4xe0Y+ECyqaAayWh3iMaofu6vuCBUPubNDftst8YX2t0oALQyj
-rX+B3X+Z46L4gYa01vQc0fhmyAo1fnTd7K0FBZ9dKeTOqpJlWZvqmGiJy1f5K0ER
-4zFbfQp+ue2PBKtbxoYKaXHOtoqnNI0sbMVPEa669RMFCSPH9LPOomqYJK+RvknV
-AqgxWxA/OeIcUhTOSfcoGbtzuPSsLcbF8GJTObdkiLPkzvz+CNi+7RSHToxwlY0C
-AwEAATANBgkqhkiG9w0BAQsFAAOCAQEAmMCI4DDOZvk9QqiBp14OTdJslBTHSsfm
-eo7oLrfHDUl7r1UDL+R858VJFTwiA388NYoOzv99nDdloTff+7hl4+e8lovYySDR
-GXK8UE0GEheiukrnx2gJLyIPVRutiOlBoR2MTusFx3weTshQEu+O57dmCmCHdXWV
-XMKz2oXKeelf84cp9O0rpWXuhl7Bp2Tfecs5P3QzIw27lPznVpY//TDKRQ5zhd3V
-GtdI8OnsOuCMmBFnfgSsDT3VIf/usgvY2LDlfRJh74uqfALs9ahgI2t0X/xW7IBl
-VVe4isaH4aGu7Q3n+AxVoIOrli7Nr52DpOQ26zJZecUQ8ZP32MnGyQ==
------END CERTIFICATE-----
-EOT
-  ]
+  path                   = var.root_path
+  type                   = "jwt"
+  description            = var.auth_backend_desccription
+  jwt_validation_pubkeys = [file("cert.pem")]
 }
 
 resource "vault_jwt_auth_backend_role" "app_role" {
-  backend        = vault_jwt_auth_backend.psjwt.path
-  role_name      = "role-${var.application_name}"
-  token_policies = [vault_policy.app_policy.name]
+  backend         = vault_jwt_auth_backend.psjwt.path
+  role_name       = "role-${var.application_name}"
+  token_policies  = [vault_policy.app_policy.name]
   role_type       = "jwt"
   bound_subject   = var.application_name
   bound_audiences = ["vault"]
--- policies.tf
+++ policies.tf
@@ -1,5 +1,5 @@
 resource "vault_policy" "app_policy" {
-  name = "policy-${var.application_name}"
+  name   = "policy-${var.application_name}"
   policy = <<EOT
 path "${var.root_path}/data/${var.application_name}" {
   capabilities = ["read"]
